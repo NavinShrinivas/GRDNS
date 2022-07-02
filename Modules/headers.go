44ae89e0459c7443f83cc45dad4bcfd749742167
NavinShrinivas <karupal2002@gmail.com>
2022-07-02 12:19:49 +0530

    Basic refactoring
    Signed-off-by: NavinShrinivas <karupal2002@gmail.com>
    
    - Small improvements to database connection
    - Better commenting
    
    Up next, flow review and rolling window

--- Modules/headers.go
+++ Modules/headers.go
@@ -1,4 +1,4 @@
-//This file has all the struct definitions and globals
+//This file has all the struct definitions, globals and properties
 
 package Modules 
 
@@ -13,42 +13,42 @@ var Record_number int64; //Very very bad idea, will definetly break down the lin
 //But I cant see any other way of tracking mutiple records for the same domain.
 //On the other hand, we dont even have that many ip addresses (atleast not in the ip4 space)
 
-var domain_map = make(map[string][]int64) //Will act as a map to the respective records for each domain
+var domain_map = make(map[string][]int64) //Will act as a map to the respective records for each domain, in memory map, leading to faster resolve times.
 
 var Addr = net.UDPAddr{
     Port: 53,
     IP:   net.ParseIP("0.0.0.0"),
 }
 
-var Work_Group sync.WaitGroup;
+var Work_Group sync.WaitGroup; //syn groups
 
 type ResponseStruct struct{
-    Name string  //For ease of use
-    Typ string //"
-    Class string //"
-    Reply string //"
-    Ttl string //"
-    Rawstr string //For creating packets in the end 
-    Rawname string  //"
-    Rawrrtype uint16  //"
-    Rawclass uint16 //"
-    Rawttl uint32 //"
-    Rawrdlength uint16 //"
+    Name string  
+    Typ string 
+    Class string 
+    Reply string 
+    Ttl string 
+    Rawstr string
+    Rawname string  
+    Rawrrtype uint16
+    Rawclass uint16
+    Rawttl uint32
+    Rawrdlength uint16
 }
 
 type System struct{
     FreeThreads int64 `json:",string"`
 }
 
-var System_State System;
+var System_State System;//Holds all system properties
 
-type Job struct{
+type Job struct{ //Stuct thats stored in the buffer
     buffer []byte 
     Conn *net.UDPConn 
     Caddr *net.UDPAddr
 }
 
-var Thread_channels [1024]chan Job //Cant have more than 1024 thread
+var Thread_channels [1024]chan Job //Cant have more than 1024 threads in the thread pool
 
 func CheckError(err error){
     if err!=nil{
